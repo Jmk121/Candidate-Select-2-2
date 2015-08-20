096dd2c9fbbaa05cd76f80088847279b13cb446e
jmk121 <jmk121@Ross-Laptop.local>
2015-08-20 09:34:59 -0400

    Initial Commit

---
+++ Candidate Select/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Candidate Select
+//
+//  Created by jmk121 on 8/20/15.
+//  Copyright (c) 2015 jmk121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Candidate Select/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Candidate Select
+//
+//  Created by jmk121 on 8/20/15.
+//  Copyright (c) 2015 jmk121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
